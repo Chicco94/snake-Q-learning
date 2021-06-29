e4ca9f1696ef51e7e18a43c7586941b780542b49
Enrico Cominato <enrico.cominato@gmail.com>
2021-06-29 09:36:50 +0200

    agent get action

--- agent.py
+++ agent.py
@@ -87,7 +87,19 @@ class Agent:
 		self.trainer.train_step(state, action, reward, next_state, done)
 
 	def get_action(self, state):
-		pass
+		# random moves: tradeoff exploration /exploitation
+		self.epsilon = 80 - self.n_games
+		final_move = [0,0,0]
+		if random.randint(0,200) < self.epsilon:
+			move = random.randint(0,2)
+			final_move[move] = 1
+		else:
+			state0 = torch.tensor(state, dtype=torch.float)
+			prediction = self.model.predict(state0) # prediction is still a raw value like [5.0, 2.3, 1.8]
+			move = torch.argmax(prediction).item() # get index of max value [5.0, 2.3, 1.8] -> 0
+			final_move[move] = 1
+		
+		return final_move
 
 def train():
 	plot_scores = []
